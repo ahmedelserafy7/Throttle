d79a6c30bbd3b15426abb9c5449310e14d308875
ahmedelserafy7 <ahmed.samer43@yahoo.com>
2021-02-10 08:20:16 +0200

    add throttle annotation

--- Throttle/Throttle.swift
+++ Throttle/Throttle.swift
@@ -20,7 +20,19 @@ class Throttle {
         self.queue = queue
     }
     
-    func throttle(_ block: @escaping () -> Void) {
+    
+    /// Takes block of code that will be executed later.
+      /// - Parameter block: takes a closure that takes no parameters and returns void.
+    /**
+     ## An annotation of using a throttle function:
+     
+     * *workItem.cancel*: First off, cancel any operation comes along.
+     * *workItem = DispatchWorkItem*: Initializes the workItem to execute the block of code after a given window of time.
+     * *previousRun = Date*: Refresh the `previousRun` when it executes by giving it new date.
+     * *delay*: If the time exceeds the `miniumDelay` that you should wait for, execute the function immediately; it's time to  launch the app, it just like the  launch time or the zero hour!, but if the time doesn't exceed the the given window of time, it should wait for the `minimumDelay` to come through.
+         * then the `delay` variable will be given to the `asyncAfter` to choose between  the the  current moments  to execute the task now or later.
+   */
+    func throttle(_ block: @escaping () -> ()) {
         workItem.cancel()
         workItem = DispatchWorkItem() { [weak self] in
             self?.previousRun = Date()
