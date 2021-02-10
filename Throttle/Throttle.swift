f2007742244f2775917b075e65f71e0331b83611
ahmedelserafy7 <ahmed.samer43@yahoo.com>
2021-02-10 08:56:38 +0200

    Trim useless spaces

--- Throttle/Throttle.swift
+++ Throttle/Throttle.swift
@@ -32,7 +32,7 @@ class Throttle {
      * *workItem.cancel*: First off, cancel any operation comes along.
      * *workItem = DispatchWorkItem*: Initializes the workItem to execute the block of code after a given window of time.
      * *previousRun = Date*: Refresh the `previousRun` when it executes by giving it new date.
-     * *delay*: If the time exceeds the `miniumDelay` that you should wait for, execute the function immediately; it's time to  launch the app, it just like the  launch time or the zero hour!, but if the time doesn't exceed the the given window of time, it should wait for the `minimumDelay` to come through.
+     * *delay*: If the time exceeds the `miniumDelay` that you should wait for, execute the function immediately; it's time to launch the app, it just like the launch time or the zero hour!, but if the time doesn't exceed the the given window of time, it should wait for the `minimumDelay` to come through.
          * then the `delay` variable will be given to the `asyncAfter` to choose between  the the  current moments  to execute the task now or later.
    */
     func throttle(_ block: @escaping () -> ()) {
